1bee7374f7c4ad25e49b7c0afd936d44decbfb66
pvictor <p.victor@outlook.fr>
2020-09-15 13:41:51 +0200

    price_estimate OK, model_training to be done

--- price_estimate.py
+++ price_estimate.py
@@ -6,4 +6,7 @@
 mileage = float(input("Enter mileage: "))
 theta_0 = float(input("Enter theta_0: "))
 theta_1 = float(input("Enter theta_1: "))
-print("mileage is : "+ str(theta_0 + theta_1 * mileage))
+print("price is : " + str(theta_0 + theta_1 * mileage))
+print("mileage is : " + str(mileage))
+print("theta_0 is : " + str(theta_0))
+print("theta_1 is : " + str(theta_1))
