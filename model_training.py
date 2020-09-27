39a8dc1f2b2c44a2a63a4f3aaabd02035c3681d0
pvictor <p.victor@outlook.fr>
2020-09-27 21:03:06 +0200

    starting model_training

--- model_training.py
+++ model_training.py
@@ -1,1 +1,11 @@
-#
+# model_training.py
+
+# GOAL: find theta and beta
+
+mileage = float(input("Enter mileage: "))
+theta_0 = float(input("Enter theta_0: "))
+theta_1 = float(input("Enter theta_1: "))
+
+print(str(mileage))
+print(str(theta_0))
+print(str(theta_1))
--- price_estimate.py
+++ price_estimate.py
@@ -6,6 +6,7 @@
 mileage = float(input("Enter mileage: "))
 theta_0 = float(input("Enter theta_0: "))
 theta_1 = float(input("Enter theta_1: "))
+
 print("price is : " + str(theta_0 + theta_1 * mileage))
 print("mileage is : " + str(mileage))
 print("theta_0 is : " + str(theta_0))
