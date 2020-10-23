453d6a700ca401b030223f876d64704dcd2f850c
pvictor <p.victor@outlook.fr>
2020-10-23 17:59:14 +0200

    Added data visualisation. Need to fix theta_0 (not updating properly)

--- model_training.py
+++ model_training.py
@@ -1,42 +1,30 @@
-# model_training.py
+# import sources
+from src.read_data import read_data
+from src.gradient_descent import gradient_descent
+from src.data_visualisation import data_visualisation
 
-from model_training_src.read_data import read_data
-
-# GOAL: find theta and beta
-
+# initializing variables
 theta_0 = 0
 theta_1 = 0
 prev_cost = -1
 cost = -3
 alpha = 1
 
-# Read the data from "data.csv"
-
+# Read the data from "data.csv", remove first row and calculate m
 data = read_data("data.csv")
 data.pop(0)
+#mileages to delete?
 mileages = [[1, x[0]] for x in data]
 m = len(data)
 
-# i à supprimer, slt pour tests
-i = 0
+# Gradient descent
+gradient_result = gradient_descent(cost, prev_cost, theta_0, theta_1, m, alpha, data)
+print(gradient_result)
+# Printing theta_0 and theta_1
+theta_0 = round(gradient_result[0], 4)
+theta_1 = round(gradient_result[1], 4)
+print ("theta_0 = " + str(theta_0))
+print ("theta_1 = " + str(theta_1))
 
-# DESCENTE DE GRADIENT
-# Tant qu'on n'a pas : une convergence, un coût à 0, ou une divergence
-#       1. on calcule prev_cost
-#   	2. on calcule le temp0 et temp1 simultanément
-#   	3. on assigne theta_0 et theta_1
-while (cost != 0 and cost != prev_cost and abs(cost - prev_cost) > 1):
-    i += 1
-    prev_cost = (sum([pow((theta_0 + theta_1 * int(x[0])) - int(x[1]), 2) for x in data]) / 2) / m
-    temp0 = theta_0 - alpha * (sum([theta_0 + theta_1 * int(x[0]) - int(x[1]) for x in data]) / m)
-    temp1 = theta_1 - alpha * (sum([(theta_0 + theta_1 * int(x[0]) - int(x[1])) * int(x[0]) for x in data]) / m)
-    print (temp0, temp1)
-    cost = (sum([pow((temp0 + temp1 * int(x[0])) - int(x[1]), 2) for x in data]) / 2) / m
-    if (cost < prev_cost):
-        theta_0 = temp0
-        theta_1 = temp1
-    else:
-        alpha /= 2
-    print(prev_cost, cost)
-    print(theta_0, theta_1)
-    print(i)
+# data visualisation
+data_visualisation(data, theta_0, theta_1)
---
+++ src/data_visualisation.py
@@ -0,0 +1,27 @@
+# import graphic libraries. Instal it with :
+#    python -m pip install -U pip
+#    python -m pip install -U matplotlib
+import matplotlib.pyplot as plt
+
+# DATA VISUALISATION
+def data_visualisation(data, theta_0, theta_1):
+    #    plotting the data
+    X = [int(x[0]) for x in data]
+    Y = [int(x[1]) for x in data]
+    plt.scatter(X,Y, color='red')
+    plt.title('Graphic representation', color='blue')
+    plt.xlabel('Mileage', color='blue')
+    plt.ylabel('Price', color='blue')
+    plt.axis([min(X), max(X), min(Y), max(Y)])
+    #   plotting the axis
+    #       calculating the min and max value with our hypothesis
+    x1 = min(X)
+    x2 = max(X)
+    y1 = theta_0 + x1 * theta_1
+    y2 = theta_0 + x2 * theta_1
+    x_values = [x1, x2]
+    y_values = [y1, y2]
+    #       inserting it in the graph
+    plt.plot(x_values, y_values, color='pink')
+    #   display the graph
+    plt.show()
